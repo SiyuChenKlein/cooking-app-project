c9e9dce57d05d466b237eca33d1e4d6efb6904f4
SiyuChenKlein <49852883+SiyuChenKlein@users.noreply.github.com>
2021-03-06 14:13:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/loginsystem/ConnectionHelper.java
@@ -0,0 +1,36 @@
+package com.example.loginsystem;
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+
+    Connection con;
+    String name, pass, ip, port, database;
+    @SuppressLint("NewApi")
+    public Connection connectionclass(){
+        ip="192.168.3.23";
+        database="Cooking_app";
+        name="klein";
+        pass="klein";
+        port="1433";
+
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection= null;
+        String ConnectionURL=null;
+
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL="jdbc:jtds:sqlserver://"+ip+":"+port+";"+"databasename="+database+";user="+name+";password="+pass+";";
+            connection= DriverManager.getConnection(ConnectionURL);
+        }
+        catch (Exception ex){
+            Log.e("Error", ex.getMessage());
+        }
+        return connection;
+    }
+}
